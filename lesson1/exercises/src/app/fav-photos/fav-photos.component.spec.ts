34f285b22010ba791414586886f3558f5e3473ae
chrissyschepker <66965088+chrissyschepker@users.noreply.github.com>
2021-04-15 11:59:51 -0500

    before  I start on the features

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.spec.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FavPhotosComponent } from './fav-photos.component';
 
@@ -6,7 +6,7 @@ describe('FavPhotosComponent', () => {
   let component: FavPhotosComponent;
   let fixture: ComponentFixture<FavPhotosComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FavPhotosComponent ]
     })
