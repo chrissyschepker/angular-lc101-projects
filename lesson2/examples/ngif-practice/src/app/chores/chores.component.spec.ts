7dac6bb162b462440e946945d02e7079364a106a
chrissyschepker <66965088+chrissyschepker@users.noreply.github.com>
2021-04-21 09:09:32 -0500

    Hello

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.spec.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ChoresComponent } from './chores.component';
 
@@ -6,7 +6,7 @@ describe('ChoresComponent', () => {
   let component: ChoresComponent;
   let fixture: ComponentFixture<ChoresComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ChoresComponent ]
     })
